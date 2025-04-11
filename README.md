e3826db73eb5581a03c70b3618ca51466eb20d47
Luana Souza <128157430+luanaapms@users.noreply.github.com>
2025-04-11 11:02:39 -0300

    adicionando atv RT3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RT3
 Atividade 03 - TypeScript POO
+
+Crie um módulo contendo classes, responsáveis por fornecerem métodos que realizem as operações matemáticas básicas, e coloque-o num repositório github.
+Desenvolva outro módulo que utilize o módulo das operações. Para isso execute códigos que façam os cálculos.
---
+++ out/index.js
@@ -0,0 +1,8 @@
+import { Somador, Subtrator, Multiplicador, Divisor } from 'modulort3';
+const a = 20;
+const b = 10;
+console.log('Testes:');
+console.log(`Soma: ${a} + ${b} = ${new Somador().calcular(a, b)}`);
+console.log(`Subtração: ${a} - ${b} = ${new Subtrator().calcular(a, b)}`);
+console.log(`Multiplicação: ${a} * ${b} = ${new Multiplicador().calcular(a, b)}`);
+console.log(`Divisão: ${a} / ${b} = ${new Divisor().calcular(a, b)}`);
